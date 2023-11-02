6e45d83e82fafd6e47108777d1f3171cd0bdc39c
OEURN SEYHA <seyhaoeurn920@gmail.com>
2023-11-02 11:54:42 +0700

    update

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -10,7 +10,7 @@ function Header() {
   const toggleMenubar = () => {
     setMenubar((menuBar) => !menuBar);
   };
-
+localStorage.setItem("test", "hllloeeelll")
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
     element.scrollIntoView({ behavior: "smooth" });
