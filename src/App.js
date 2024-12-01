9dea261f43c850f0526e4b523a9545fbe8eaf683
unknown <seyhaoeurn920@gmail.com>
2024-12-01 21:28:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -10,52 +10,8 @@ import Skill from "./Component/Skill";
 import Habbit from "./Component/Habbit";
 import ButtonDragDrpMenu from "./Component/ButtonDragDrpMenu";
 
-import emailjs from "emailjs-com";
-import {UAParser} from "ua-parser-js";
-import { useEffect } from "react";
-
-function getDeviceInfo() {
-  const parser = new UAParser();
-  const result = parser.getResult();
-  return {
-    browser: `${result.browser.name} ${result.browser.version}`,
-    os: `${result.os.name} ${result.os.version}`,
-    device: result.device.model || "Unknown Device",
-  };
-}
-
-function sendDeviceInfo() {
-  const deviceInfo = getDeviceInfo();
-
-  const templateParams = {
-    browser: deviceInfo.browser,
-    os: deviceInfo.os,
-    device: deviceInfo.device,
-  };
-
-  emailjs
-    .send(
-      "Seyha*8888", // Replace with your EmailJS Service ID
-      "template_5t8b8vh", // Replace with your Template ID
-      templateParams,
-      "ukOVGovFRQ568udoc9s6J" // Replace with your User ID
-    )
-    .then(
-      (response) => {
-        console.log("Email sent successfully!", response.status, response.text);
-      },
-      (err) => {
-        console.error("Failed to send email. Error:", err);
-      }
-    );
-}
-
 
 function App() {
-  
-  useEffect(() => {
-    sendDeviceInfo();
-  }, []);
   return (
     <div className="App">
       <Header />
