b1d85d00f4b42e39404f4bac15255c9d880b04d4
seyha.oeurn <seyhaoeurn920@gmail.com>
2025-02-01 16:00:15 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
 function App() {
- 
   return (
     <>
     <main className='App'>
